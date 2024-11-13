8ffc1e6581af67b49030342ceb8d2459fc6c396d
dave-fink <david@keypixel.com>
2024-11-13 17:20:28 -0500

    Initial Commit

---
+++ blocks/roadmap/roadmap.js
@@ -0,0 +1,524 @@
+/* eslint-disable no-use-before-define, object-curly-newline, function-paren-newline */
+/*import { div, ul, li, p, a, span, sup } from '../../scripts/dom-helpers.js';
+import { scrollToMe } from '../../scripts/animations.js';
+import { readBlockConfig } from '../../scripts/aem.js';
+
+// Generate all quarters between start and end
+function generateYearQuarterRange(start, end) {
+  let [startYear, startQuarter] = start.split('-');
+  let [endYear, endQuarter] = end.split('-');
+
+  // Adjust start to one quarter earlier
+  startQuarter = parseInt(startQuarter.replace('Q', ''), 10) - 1;
+  if (startQuarter === 0) {
+    startQuarter = 4;
+    startYear = parseInt(startYear, 10) - 1;
+  }
+
+  // Adjust end to one quarter later
+  endQuarter = parseInt(endQuarter.replace('Q', ''), 10) + 1;
+  if (endQuarter === 5) {
+    endQuarter = 1;
+    endYear = parseInt(endYear, 10) + 1;
+  }
+
+  // fill in anny empty quarters
+  const yearQuarterList = [];
+  let currentYear = parseInt(startYear, 10);
+  let currentQuarter = parseInt(startQuarter, 10);
+  while (
+    currentYear < parseInt(endYear, 10)
+    || (currentYear === parseInt(endYear, 10) && currentQuarter <= parseInt(endQuarter, 10))
+  ) {
+    yearQuarterList.push({ year: currentYear, quarter: `Q${currentQuarter}` });
+    currentQuarter += 1;
+    if (currentQuarter > 4) {
+      currentQuarter = 1;
+      currentYear += 1;
+    }
+  }
+  return yearQuarterList;
+}
+
+
+
+// find the earliest and latest year/quarter
+function getEarliestAndLatest(roadmapData) {
+  let earliest = null;
+  let latest = null;
+  roadmapData.forEach(({ year, quarter }) => {
+    const current = `${year}-${quarter}`;
+    if (!earliest || compareYearQuarter(current, earliest) < 0) {
+      earliest = current;
+    }
+    if (!latest || compareYearQuarter(current, latest) > 0) {
+      latest = current;
+    }
+  });
+  return { earliest, latest };
+}
+
+// compare year and quarter
+function compareYearQuarter(yq1, yq2) {
+  const [year1, quarter1] = yq1.split('-').map((v) => parseInt(v.replace('Q', ''), 10));
+  const [year2, quarter2] = yq2.split('-').map((v) => parseInt(v.replace('Q', ''), 10));
+  if (year1 < year2 || (year1 === year2 && quarter1 < quarter2)) return -1;
+  if (year1 > year2 || (year1 === year2 && quarter1 > quarter2)) return 1;
+  return 0;
+}
+
+function fixYears(block, years) {
+  // get center position
+  const leftPosition = (window.innerWidth / 2) - 50;
+
+  block.addEventListener('scroll', () => {
+    const scrollLeftPos = block.scrollLeft;
+
+    years.forEach((year) => {
+      const yearLeftPos = year.offsetLeft;
+      if (scrollLeftPos >= yearLeftPos - leftPosition) {
+        year.classList.add('fixed');
+      } else {
+        year.classList.remove('fixed');
+      }
+    });
+  });
+}
+
+export default function decorate(block) {
+  const blockConfig = readBlockConfig(block);
+
+  // Calculate the current year and quarter
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth();
+  const currentQuarter = `Q${Math.floor(currentMonth / 3) + 1}`;
+
+  let activePos;
+  let yIndex = 0;
+  let qIndex = 0;
+  let pTabIndex = 1;
+
+  block.innerHTML = '';
+
+  fetch(blockConfig['data-source'])
+    .then((response) => response.json())
+    .then((data) => {
+      const roadmapData = data.data;
+      const { earliest, latest } = getEarliestAndLatest(roadmapData);
+      const fullYearQuarterRange = generateYearQuarterRange(earliest, latest);
+
+      // Group data by year and quarter
+      // eslint-disable-next-line max-len
+      const groupData = roadmapData.reduce((acc, { year, quarter, project, description, path, sort, btn }) => {
+        acc[year] = acc[year] || {};
+        acc[year][quarter] = acc[year][quarter] || [];
+        acc[year][quarter].push({ title: project, tip: description, path, sort, btn });
+
+        // Sort the projects for this quarter by the sort number
+        acc[year][quarter].sort((A, B) => A.sort - B.sort);
+
+        return acc;
+      }, {});
+
+      const $heading = div({ class: 'heading' },
+        'AMS SSA evolution ',
+        span('(roadmap)'),
+      );
+
+      const $disclaimer = div({ class: 'disclaimer' },
+        'EA = Early Availability',
+        span(' | '),
+        'GA = General Availability',
+      );
+
+      const $years = ul({ class: 'years' });
+
+      fullYearQuarterRange.forEach(({ year, quarter }) => {
+        let $year = $years.querySelector(`[data-year="${year}"]`);
+        if (!$year) {
+          $year = li({ class: `y clr-${yIndex}`, 'data-year': year }, '\u00A0', div(year));
+          yIndex += 1; // Increment only for new years
+        }
+
+        let $quarters = $year.querySelector('ul.quarters');
+        if (!$quarters) {
+          $quarters = ul({ class: 'quarters' });
+        }
+
+        // Increment the position index for each quarter
+        qIndex += 1;
+        const pos = qIndex;
+
+        // Create the quarter element and attach it
+        const $quarter = li({ class: 'q', 'data-i': pos }, quarter);
+
+        // Initial start quarter (current date)
+        if (parseInt(year, 10) === currentYear && quarter === currentQuarter) {
+          $quarter.classList.add('start');
+          activePos = pos;
+        }
+
+        // Retrieve projects for the corresponding year/quarter if they exist
+        const projects = (groupData[year] && groupData[year][quarter]) || [];
+        const $projects = ul({ class: 'projects' });
+
+        // Iterate over the projects and append them to the quarter
+        projects.forEach(({ title, tip, path, btn }, n) => {
+          // Ignore empty projects
+          if (title === '') return;
+
+          // Process title for (EA) and (GA)
+          const suffixMap = {
+            '(EA)': { title: 'Early Access', label: 'EA' },
+            '(GA)': { title: 'General Access', label: 'GA' },
+          };
+          const suffix = Object.keys(suffixMap).find((key) => title.includes(key));
+          let $pTitle = suffix ? title.replace(suffix, '') : title;
+          const suffixElement = suffix ? sup({ title: suffixMap[suffix].title }, suffixMap[suffix].label) : '';
+
+          $pTitle = span($pTitle, suffixElement);
+
+          const $learnMoreLink = btn.toLowerCase() !== 'hide' ? p(a({ class: 'btn', href: path }, 'Learn more')) : '';
+
+          // Create the project element and append it to the project list
+          const $project = li({ class: 'p', style: `--index:${n}`, tabindex: pTabIndex },
+            div($pTitle,
+              div({ class: 'tooltip' },
+                div(tip,
+                  $learnMoreLink,
+                ),
+              ),
+            ),
+          );
+
+          $project.addEventListener('click', () => {
+            $years.querySelectorAll('.active').forEach(($p) => $p.classList.remove('active'));
+            $project.classList.toggle('active');
+          });
+
+          // Expand on focus
+          $project.addEventListener('focus', () => {
+            $years.querySelectorAll('.active').forEach(($p) => $p.classList.remove('active'));
+            $project.classList.add('active');
+          });
+
+          // Optional: collapse on blur
+          $project.addEventListener('blur', () => {
+            $project.classList.remove('active');
+          });
+
+          $projects.appendChild($project);
+          pTabIndex += 1; // Increment only for new years
+        });
+
+        $years.appendChild($year);
+        $year.appendChild($quarters);
+        $quarters.appendChild($quarter);
+        $quarter.appendChild($projects);
+      });
+
+      const quarterObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) entry.target.classList.add('on');
+          else entry.target.classList.remove('on');
+        });
+      }, {
+        threshold: [0.40], root: $years,
+      });
+      $years.querySelectorAll('.q').forEach(($quarter) => {
+        quarterObserver.observe($quarter);
+      });
+
+      function scroll(dir) {
+        // return if the button is disabled
+        if (
+          (dir === -1 && activePos === 1) // is at start
+          || (dir === 1 && activePos === qIndex) // is at end
+        ) return;
+
+        activePos += dir;
+        const target = block.querySelector(`[data-i="${activePos}"]`);
+
+        if (target) scrollToMe($years, target, 500);
+
+        // Toggle disabled state for left and right buttons
+        $left.classList.toggle('disabled', activePos === 1);
+        $right.classList.toggle('disabled', activePos === qIndex);
+
+        // Close all active projects
+        // $years.querySelectorAll('.active').forEach(($p) => $p.classList.remove('active'));
+      }
+      const $left = div({ class: 'left' }, div());
+      const $right = div({ class: 'right' }, div());
+      $left.addEventListener('click', () => scroll(-1));
+      $right.addEventListener('click', () => scroll(1));
+
+      // keyboard navigation
+      document.addEventListener('keydown', (e) => {
+        if (e.key === 'ArrowRight') scroll(1);
+        if (e.key === 'ArrowLeft') scroll(-1);
+      });
+
+      const $timeline = div({ class: 'timeline' }, $years, $left, $right);
+
+      block.append($heading, $timeline, $disclaimer);
+
+      // scroll to start for initial scroll
+      const roadMapObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const startHere = block.querySelector('.start');
+            scrollToMe($years, startHere, 2000);
+            // stop observing
+            observer.disconnect();
+          }
+        });
+      }, {
+        threshold: [0.20],
+      });
+      roadMapObserver.observe($timeline);
+
+      fixYears($years, $timeline.querySelectorAll('.y'));
+    })
+    .catch((error) => {
+      // eslint-disable-next-line no-console
+      console.error('Error fetching roadmap data:', error);
+    });
+}*/
+
+
+
+//Test Code
+/* eslint-disable no-use-before-define, object-curly-newline, function-paren-newline */
+import { div, ul, li, p, a, span, sup } from '../../scripts/dom-helpers.js';
+import { scrollToMe } from '../../scripts/animations.js';
+import { readBlockConfig } from '../../scripts/aem.js';
+
+// Generate all years between start and end
+function generateYearRange(start, end) {
+  let startYear = parseInt(start, 10);
+  let endYear = parseInt(end, 10);
+
+  // Adjust start to one year earlier
+  //startYear -= 1;
+
+  // Adjust end to one year later
+  //endYear += 1;
+
+  // Fill in any empty years
+  const yearList = [];
+  for (let year = startYear; year <= endYear; year += 1) {
+    yearList.push(year);
+  }
+  return yearList;
+}
+
+// Find the earliest and latest year
+function getEarliestAndLatest(roadmapData) {
+  let earliest = null;
+  let latest = null;
+  roadmapData.forEach(({ year }) => {
+    if (!earliest || year < earliest) {
+      earliest = year;
+    }
+    if (!latest || year > latest) {
+      latest = year;
+    }
+  });
+  return { earliest, latest };
+}
+
+function fixYears(block, years) {
+  // Get center position
+  const leftPosition = (window.innerWidth / 2) - 50;
+
+  block.addEventListener('scroll', () => {
+    const scrollLeftPos = block.scrollLeft;
+
+    years.forEach((year) => {
+      const yearLeftPos = year.offsetLeft;
+      if (scrollLeftPos >= yearLeftPos - leftPosition) {
+        year.classList.add('fixed');
+      } else {
+        year.classList.remove('fixed');
+      }
+    });
+  });
+}
+
+export default function decorate(block) {
+  const blockConfig = readBlockConfig(block);
+
+  // Calculate the current year
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+
+  let activePos = 1;
+  let yIndex = 0;
+  let pTabIndex = 1;
+
+  block.innerHTML = '';
+
+  fetch(blockConfig['data-source'])
+    .then((response) => response.json())
+    .then((data) => {
+      const roadmapData = data.data;
+      const { earliest, latest } = getEarliestAndLatest(roadmapData);
+      const fullYearRange = generateYearRange(earliest, latest);
+
+      // Group data by year
+      const groupData = roadmapData.reduce((acc, { year, project, description, path, sort, btn }) => {
+        acc[year] = acc[year] || [];
+        acc[year].push({ title: project, tip: description, path, sort, btn });
+
+        // Sort the projects for this year by the sort number
+        acc[year].sort((A, B) => A.sort - B.sort);
+
+        return acc;
+      }, {});
+
+      const $heading = div({ class: 'heading' },
+        'AMS SSA evolution ',
+       /* span('(roadmap)'),*/
+      );
+
+      const $disclaimer = div({ class: 'disclaimer' },
+        'EA = Early Availability',
+        span(' | '),
+        'GA = General Availability',
+      );
+
+      const $years = ul({ class: 'years' });
+
+      fullYearRange.forEach((year) => {
+        let $year = $years.querySelector(`[data-year="${year}"]`);
+        if (!$year) {
+          $year = li({ class: `y clr-${yIndex}`, 'data-year': year }, '\u00A0', div(year));
+          yIndex += 1; // Increment only for new years
+        }
+
+        // Retrieve projects for the corresponding year if they exist
+        const projects = groupData[year] || [];
+        const $projects = ul({ class: 'projects' });
+
+        // Iterate over the projects and append them to the year
+        projects.forEach(({ title, tip, path, btn }, n) => {
+          // Ignore empty projects
+          if (title === '') return;
+
+          // Process title for (EA) and (GA)
+          const suffixMap = {
+            '(EA)': { title: 'Early Access', label: 'EA' },
+            '(GA)': { title: 'General Access', label: 'GA' },
+          };
+          const suffix = Object.keys(suffixMap).find((key) => title.includes(key));
+          let $pTitle = suffix ? title.replace(suffix, '') : title;
+          const suffixElement = suffix ? sup({ title: suffixMap[suffix].title }, suffixMap[suffix].label) : '';
+
+          $pTitle = span($pTitle, suffixElement);
+
+          const $learnMoreLink = btn.toLowerCase() !== 'hide' ? p(a({ class: 'btn', href: path }, 'Learn more')) : '';
+
+          // Create the project element and append it to the project list
+          const $project = li({ class: 'p', style: `--index:${n}`, tabindex: pTabIndex },
+            div($pTitle,
+              div({ class: 'tooltip' },
+                div(tip,
+                  $learnMoreLink,
+                ),
+              ),
+            ),
+          );
+
+          $project.addEventListener('click', () => {
+            $years.querySelectorAll('.active').forEach(($p) => $p.classList.remove('active'));
+            $project.classList.toggle('active');
+          });
+
+          // Expand on focus
+          $project.addEventListener('focus', () => {
+            $years.querySelectorAll('.active').forEach(($p) => $p.classList.remove('active'));
+            $project.classList.add('active');
+          });
+
+          // Optional: collapse on blur
+          $project.addEventListener('blur', () => {
+            $project.classList.remove('active');
+          });
+
+          $projects.appendChild($project);
+          pTabIndex += 1; // Increment only for new years
+        });
+
+        $years.appendChild($year);
+        $year.appendChild($projects);
+      });
+
+      const yearObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) entry.target.classList.add('on');
+          else entry.target.classList.remove('on');
+        });
+      }, {
+        threshold: [0.40], root: $years,
+      });
+      $years.querySelectorAll('.y').forEach(($year) => {
+        yearObserver.observe($year);
+      });
+
+      function scroll(dir) {
+        // return if the button is disabled
+        // Adjust activePos within bounds
+  activePos = Math.max(1, Math.min(yIndex, activePos + dir));
+
+  // Find the year element with the data-year matching the activePos year
+  const target = block.querySelector(`[data-year="${fullYearRange[activePos - 1]}"]`);
+
+  if (target) {
+    // Smooth scroll to the targeted year
+    scrollToMe($years, target, 500);
+
+    // Update the disabled state of buttons based on active position
+    $left.classList.toggle('disabled', activePos === 1);
+    $right.classList.toggle('disabled', activePos === yIndex);
+  }
+      }
+      // Event listeners for the left and right scroll buttons
+      const $left = div({ class: 'left' }, div());
+      const $right = div({ class: 'right' }, div());
+      $left.addEventListener('click', () => scroll(-1));
+      $right.addEventListener('click', () => scroll(1));
+
+      // keyboard navigation
+      document.addEventListener('keydown', (e) => {
+        if (e.key === 'ArrowRight') scroll(1);
+        if (e.key === 'ArrowLeft') scroll(-1);
+      });
+
+      const $timeline = div({ class: 'timeline' }, $years, $left, $right);
+
+      block.append($heading, $timeline, $disclaimer);
+
+      // scroll to start for initial scroll
+      const roadMapObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const startHere = block.querySelector(`[data-year="${currentYear}"]`);
+            scrollToMe($years, startHere, 2000);
+            // stop observing
+            observer.disconnect();
+          }
+        });
+      }, {
+        threshold: [0.20],
+      });
+      roadMapObserver.observe($timeline);
+
+      fixYears($years, $timeline.querySelectorAll('.y'));
+    })
+    .catch((error) => {
+      // eslint-disable-next-line no-console
+      console.error('Error fetching roadmap data:', error);
+    });
+}
---
+++ scripts/scripts.js
@@ -0,0 +1,199 @@
+import {
+  loadHeader,
+  loadFooter,
+  decorateButtons,
+  decorateIcons,
+  decorateSections,
+  decorateBlocks,
+  decorateTemplateAndTheme,
+  waitForFirstImage,
+  loadSection,
+  loadSections,
+  loadCSS,
+  sampleRUM,
+  getMetadata,
+} from './aem.js';
+
+function autolinkModals(element) {
+  element.addEventListener('click', async (e) => {
+    const origin = e.target.closest('a');
+
+    if (
+      origin
+      && origin.href
+      && (origin.href.includes('/modals/') || origin.href.includes('/learnmore/'))
+    ) {
+      e.preventDefault();
+      const { openModal } = await import(`${window.hlx.codeBasePath}/blocks/modal/modal.js`);
+      openModal(origin.href);
+    }
+  });
+}
+
+/**
+ * load fonts.css and set a session storage flag
+ */
+async function loadFonts() {
+  await loadCSS(`${window.hlx.codeBasePath}/styles/fonts.css`);
+  try {
+    if (!window.location.hostname.includes('localhost')) sessionStorage.setItem('fonts-loaded', 'true');
+  } catch (e) {
+    // do nothing
+  }
+}
+
+/**
+ * decorates external links to open in new window
+ */
+function decorateAnchors(element) {
+  const A = element.getElementsByTagName('a');
+  const As = Array.from(A).filter((a) => (a.href && !a.href.match(`^http[s]*://${window.location.host}/`)));
+  if (As.length) {
+    As.forEach((a) => {
+      a.target = '_blank';
+    });
+  }
+}
+
+/**
+ * Decorates the main element.
+ * @param {Element} main The main element
+ */
+// eslint-disable-next-line import/prefer-default-export
+export function decorateMain(main) {
+  // hopefully forward compatible button decoration
+  decorateButtons(main);
+  decorateIcons(main);
+  decorateAnchors(main);
+  decorateSections(main);
+  decorateBlocks(main);
+}
+
+/**
+ * Loads everything needed to get to LCP.
+ * @param {Element} doc The container element
+ */
+async function loadEager(doc) {
+  document.documentElement.lang = 'en';
+  // Detect the user's language or fallback to 'en' if unavailable
+  //const userLang = navigator.language || navigator.userLanguage || 'en';
+  //document.documentElement.lang = userLang;
+
+  // Create an object mapping languages to URLs
+//const urlMap = {
+  //en: '/en/',  // English content path
+  //fr: '/fr/',  // French content path
+  //de: '/de/',  // German content path
+  //default: '/' // Default path 
+//};
+
+// Function to get the content path based on language
+//function getContentPathForLang(language) {
+  //const baseLang = language.split('-')[0]; // Extract base language (e.g., 'en' from 'en-US')
+  //return urlMap[baseLang] || urlMap.default; // Fallback to 'default' if language not in the map
+//}
+
+// Remove language-based redirection
+// Get the content path for the user's language
+//const contentPath = getContentPathForLang(userLang);
+
+// Perform a soft redirect only if the current path is different
+//if (window.location.pathname !== contentPath) {
+  // Use history.pushState to change the URL without reloading the page
+  //window.history.pushState(null, '', contentPath);
+
+  // Optionally, load new content for the current language
+  //await loadContent(contentPath);
+//}
+
+  decorateTemplateAndTheme();
+  const main = doc.querySelector('main');
+  if (main) {
+    decorateMain(main);
+    document.body.classList.add('appear');
+    await loadSection(main.querySelector('.section'), waitForFirstImage);
+  }
+
+  sampleRUM.enhance();
+
+  try {
+    /* if desktop (proxy for fast connection) or fonts already loaded, load fonts.css */
+    if (window.innerWidth >= 900 || sessionStorage.getItem('fonts-loaded')) {
+      loadFonts();
+    }
+  } catch (e) {
+    // do nothing
+  }
+}
+
+// Function to dynamically load new content for the soft redirect
+async function loadContent(contentPath) {
+  try {
+    // Fetch the content for the given language path (e.g., '/en/', '/fr/')
+    const response = await fetch(`${window.hlx.codeBasePath}${contentPath}content.json`);
+    
+    if (response.ok) {
+      const contentData = await response.json();
+      
+      // Update the main content of the page without reloading
+      const main = document.querySelector('main');
+      if (main) {
+        main.innerHTML = ''; // Clear existing content
+        // Append new content (based on your actual content structure)
+        main.innerHTML = contentData.html; 
+      }
+    } else {
+      console.error('Error fetching content:', response.status);
+    }
+  } catch (error) {
+    console.error('Failed to load content:', error);
+  }
+}
+
+/**
+ * Loads everything that doesn't need to be delayed.
+ * @param {Element} doc The container element
+ */
+async function loadLazy(doc) {
+  autolinkModals(doc);
+
+  const main = doc.querySelector('main');
+  await loadSections(main);
+
+  const { hash } = window.location;
+  const element = hash ? doc.getElementById(hash.substring(1)) : false;
+  if (hash && element) element.scrollIntoView();
+
+  const $header = doc.querySelector('header');
+  const $footer = doc.querySelector('footer');
+
+  if (window.self !== window.top) {
+    // if in iframe remove header and footer
+    $header.remove();
+    $footer.remove();
+  } else {
+    await loadHeader($header);
+    await loadFooter($footer);
+  }
+
+  loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
+  loadFonts();
+}
+
+/**
+ * Loads everything that happens a lot later,
+ * without impacting the user experience.
+ */
+function loadDelayed() {
+  // eslint-disable-next-line import/no-cycle
+  window.setTimeout(() => import('./delayed.js'), 3000);
+  // load anything that can be postponed to the latest here
+}
+
+async function loadPage() {
+  await loadEager(document);
+  await loadLazy(document);
+  loadDelayed();
+}
+
+loadPage();
